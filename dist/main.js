720d344533e724fb553da67b16af9c50af3610dd
tuana <tuananh45468@gmail.com>
2023-04-29 12:34:03 +0700

    complete desktop version

--- dist/main.js
+++ dist/main.js
@@ -2,6 +2,14 @@ const ratingBtn = document.getElementsByClassName("rating");
 
 for (let button of ratingBtn) {
   button.addEventListener("click", (event) => {
+    // Remove the selected class and reset the styles of all buttons
+    for (let otherButton of ratingBtn) {
+      otherButton.classList.remove("selected");
+      otherButton.classList.remove("bg-orange", "text-white");
+    }
+
+    event.target.classList.add("selected", "bg-orange", "text-white");
+
     window.selectedRating = event.target.textContent;
   });
 }
